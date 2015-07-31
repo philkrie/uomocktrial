fc62fe34ff78b4f46587e8811c3cf00e7e44b1af
Phillip Kriegel <philkrie@gmail.com>
2015-07-31 01:52:39 -0600

    major UI enhancement, especially for small screens and mobile. utilizes angular js to inject common nav bar, will be used for just about everything to make files cleaner;

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+/*global angular */
+(function () {
+    'use strict';
+    var app = angular.module('uomocktrial', []);
+
+    app.controller('MockController', function () {
+    
+    });
+    
+}());
